bc6193db43614e59617d1cbc6a1fb51f64cbf76e
Evgenii Kortukov <elephunker1@gmail.com>
2022-11-23 12:34:45 +0100

    Add red_dim param to config

--- policies/generation.py
+++ policies/generation.py
@@ -14,13 +14,15 @@ def policy_generation(bandit, reduct_matrix, params):
     if bandit == 'BCMAB_RP':
         gamma = params.get("gamma", 0.999)
         a = params.get("a", 0.2)
-        policy = BCMAB_RP(org_dim, red_dim,  a=a, gamma=gamma)
+        red_dim_param = params.get("red_dim", red_dim)
+        policy = BCMAB_RP(org_dim, red_dim_param,  a=a, gamma=gamma)
     elif bandit == 'BCMABRP_Old':
         nu = params.get("nu", 0.5)
         policy = BCMABRP_Old(org_dim, red_dim, reduct_matrix, delta=0.5, R=0.01, lambd=0.5, nu=nu)
     elif bandit == 'CBRAP':
         alpha = params.get("alpha", 0.5)
-        policy = CBRAP(org_dim, red_dim, alpha=alpha)
+        red_dim_param = params.get("red_dim", red_dim)
+        policy = CBRAP(org_dim, red_dim_param, alpha=alpha)
     elif bandit == 'LinearTS':
         nu = params.get("nu", 0.5)
         policy = LinearTS(org_dim, delta=0.5, R=0.01, epsilon=0.5, nu=nu)
