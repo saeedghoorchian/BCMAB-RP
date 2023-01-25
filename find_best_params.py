9587d92ed036883099e4f4318963b44f73c824bb
Evgenii Kortukov <elephunker1@gmail.com>
2023-01-25 22:35:31 +0100

    Update code for plotting and amazon params

--- find_best_params.py
+++ find_best_params.py
@@ -4,13 +4,13 @@ import numpy as np
 from pathlib import Path
 
 # Enter the dimensions for the dataset you use now. These are for Jester.
-DIMENSIONS = [25, 50, 100, 250, 500]
+DIMENSIONS = [10, 20, 50, 100, 200]
 NUM_PARAMS = 72
 
 all_rewards = np.zeros((len(DIMENSIONS), NUM_PARAMS))
 for i, dim in enumerate(DIMENSIONS):
     p = Path('.')
-    paths = list(p.glob(f'*cbrap*d_{dim}.json'))
+    paths = list(p.glob(f'*bcmabrp*d_{dim}.json'))
     values = []
     for pat in paths:
         with open(pat, 'r') as f:
@@ -30,13 +30,14 @@ best_param = keys[best_ind]
 
 # Additional for deepfm calculations
 p = Path('.')
-paths = list(p.glob('results_jester_config_*_t_30000_n_3_d_300.json'))
+paths = list(p.glob('results_amazon_config_*_t_30000_n_3_d_15.json'))
 rewards = np.zeros(len(paths))
 for pat in paths:
+    print(pat)
     ind = int(str(pat).split('config_')[1].split('_t_30000')[0])
     with open(pat, 'r') as f:
         tune_dict = json.load(f)
-        print(ind, tune_dict[0][1]['Total reward mean'])
+        # print(ind, tune_dict[0][1]['Total reward mean'])
         rewards[ind] = tune_dict[0][1]['Total reward mean']
 best_reward = rewards.max()
 best_ind = rewards.argmax()
